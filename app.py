0a593f32191968a2aa05fb81d6d9f51ad00efa0c
Uwila <uwila@runbox.com>
2021-07-30 00:53:23 +0200

    do db to dict conversion in separate method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, g
+from flask import Flask, g, request
 from flask.json import jsonify
 import sqlite3
 
@@ -24,38 +24,40 @@ def show_index():
     return '<a href="/artists">Artists</a><br><a href="/songs">Songs</a>'
 
 
+def row_to_artist(row):
+    # This translates a database row from artists table to a dict
+    return {
+        "id": row[0],
+        "name": row[1],
+    }
+
+
 @app.route("/artists")
 def hello_world():
     db = get_db()
-    artists = [
-        {
-            "id": row[0],
-            "name": row[1],
-        }
-        for row in db.execute("SELECT * FROM artists")
-    ]
+    artists = [row_to_artist(row) for row in db.execute("SELECT * FROM artists")]
 
     return jsonify(artists)
+
+
+def row_to_song(row):
+    # This translates a database row from artists table to a dict
+    return {
+        "id": row[0],
+        "name": row[1],
+        "year": row[2],
+        "artist": row[3],
+        "shortname": row[4],
+        "bpm": row[5],
+        "duration": row[6],
+        "genre": row[7],
+        "spotify_id": row[8],
+        "album": row[9],
+    }
 
 
 @app.route("/songs")
 def list_songs():
     db = get_db()
-    songs = [
-        {
-            "id": row[0],
-            "name": row[1],
-            "year": row[2],
-            "artist": row[3],
-            "shortname": row[4],
-            "bpm": row[5],
-            "duration": row[6],
-            "genre": row[7],
-            "spotify_id": row[8],
-            "album": row[9],
-        }
-        for row in db.execute("SELECT * FROM songs")
-    ]
-
+    songs = [row_to_song(row) for row in db.execute("SELECT * FROM songs")]
     return jsonify(songs)
-
